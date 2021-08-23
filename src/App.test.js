8c350828452930972ac93956eee4f8008e89bd01
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-23 20:39:20 +0300

    init with few tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
+import LessonList from "./LessonList";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders App without crashing", () => {
+    const appWrapper = shallow(<App />);
+  });
+
+  it("renderes a lesson list", () => {
+    const appWrapper = shallow(<App />);
+    const lessonList = appWrapper.find(LessonList);
+
+    expect(lessonList).toHaveLength(1);
+  });
+
+  it("", () => {
+    const appWrapper = shallow(<App />);
+    const appState = appWrapper.state();
+  });
 });
